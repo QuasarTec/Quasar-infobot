dcee86e409fc7f04e07f37be6636a167ea223ea6
QuasarTec <easy.stars@mail.ru>
2021-06-10 13:18:29 +0300

    fix promo packages

--- server/controllers/admin-panel/promo.js
+++ server/controllers/admin-panel/promo.js
@@ -6,20 +6,28 @@ module.exports = (req, res) => {
         type
     } = req.body;
 
-    var licenses;
+    var licenses,
+        limitDay;
 
     if (type === 'p1') {
         licenses = 500;
+        limitDay = 17;
     } else if (type === 'p2') {
         licenses = 700;
+        limitDay = 24;
     } else if (type === 'p3') {
         licenses = 1000;
+        limitDay = 40;
     }
 
     usernames.forEach(el => {
-        const query = `INSERT INTO Licensi (\`Username\`, \`Timeset\`,\` Activate\`, \`ActivateLicens\`, \`LimitDay\`, \`Limit\`) VALUES ('${el}', ${new Date().toUTCString()}, 1, true, 40, ${licenses});`;
+        if (el[0] !== "@") {
+            el = "@" + el;
+        }
 
-        //client.query(query);
+        const query = `INSERT INTO Licensi (\`Username\`, \`Timeset\`, \`Activate\`, \`ActivateLicens\`, \`LimitDay\`, \`Limit\`) VALUES ('${el}', '${new Date().toJSON().slice(0, 10)}', 1, true, ${limitDay}, ${licenses});`;
+
+        client.query(query);
     })
 
 
