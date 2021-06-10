faea4ed28b20053ddb3ab9949998672edc09136f
QuasarTec <easy.stars@mail.ru>
2021-06-10 12:38:41 +0300

    add check on payed connect, in getting gift func

--- commands/license/get_license.js
+++ commands/license/get_license.js
@@ -22,35 +22,40 @@ module.exports = async (bot,msg) => {
         return;
     }
 
+    if (await checkConnectNotPayed(msg.chat.id)) {
+        bot.sendMessage(msg.chat.id, 'Этот подарок могут забрать только те пользователи, кто проплатил сервис Connect', INLINE_OPTIONS);
+        return;
+    }
+
     let inviter = await getInviterName(msg.chat.username);
 
     let query = `SELECT * FROM \`Licensi\` WHERE Username = ${inviter}`;
 
-    let res = await client_mysql.query(query)
+    let user = await client_mysql.query(query)
 
-    if (res.length === 0) {
+    if (user.length === 0) {
         bot.sendMessage(msg.chat.id, 'Похоже, что у вашего пригласителя нет подарочных лицензий (Пригласитель тот, кто вас пригласил, а не тот, кого вам выдаёт бот за пригласителя. Хотя иногда это идинаковые люди)', INLINE_OPTIONS);
         return;
     }
 
-    if (res[0].Limit <= 0) {
-        if (res[1] !== undefined) {
-            let delete_package = `DELETE FROM \`Licensi\` WHERE id = ${res[0].id}`;
+    if (user[0].Limit <= 0) {
+        if (user[1] !== undefined) {
+            let delete_package = `DELETE FROM \`Licensi\` WHERE id = ${user[0].id}`;
 
             await client_mysql.query(delete_package);
 
-            res.shift();
+            user.shift();
         } else {
             bot.sendMessage(msg.chat.id, 'Похоже, что у вашего пригласителя нет подарочных лицензий (Пригласитель тот, кто вас пригласил, а не тот, кого вам выдаёт бот за пригласителя. Хотя иногда это идинаковые люди)', INLINE_OPTIONS);
         }
     }		
 
-    if (parseInt((new Date()-res[0].Timeset)/(24*3600*1000)) > res[0].LimitDay) {
+    if (parseInt((new Date()-user[0].Timeset)/(24*3600*1000)) > user[0].LimitDay) {
         bot.sendMessage(msg.chat.id, 'Упс, похоже вы опоздали(((', INLINE_OPTIONS);
         return;
     }
 
-    if (res[0].ActivateLicens === 'false') {
+    if (user[0].ActivateLicens === 'false') {
         bot.sendMessage(msg.chat.id, 'Похоже у вашего пригласителя не активирована раздача', INLINE_OPTIONS);
         return;
     }
@@ -61,7 +66,7 @@ module.exports = async (bot,msg) => {
 
     await client.query(set_gift_recived);
 
-    let decriment_gifts = `UPDATE \`Licensi\` SET Limit=Limit-1 WHERE id = ${res[0].id}`;
+    let decriment_gifts = `UPDATE \`Licensi\` SET Limit=Limit-1 WHERE id = ${user[0].id}`;
 
     await client_mysql.query(decriment_gifts);
 }
@@ -78,12 +83,20 @@ const checkDate = async (chat_id, username) => {
     return start_date.getTime() > registration_date.getTime() && username !== 'jnecua123';
 }
 
-const checkRecived = async (chat_id, username) => {
+const checkRecived = async (chat_id) => {
     let gift_recived = `SELECT gift_recived FROM quasar_telegrambot_users_new WHERE chat_id = ${chat_id};`;
 
     let res = await client.query(gift_recived);
 
     return res.rowCount > 0 ? res.rows[0].gift_recived : false;
+}
+
+const checkConnectNotPayed = async (chat_id) => {
+    let payed_query = `SELECT last_pay FROM quasar_telegrambot_users_new WHERE chat_id = ${chat_id}`;
+
+    let payed = await client.query(payed_query);
+
+    return payed.rowCount > 0 ? parseInt((new Date()-payed.rows[0].last_pay)/(24*3600*1000)) > 30 : false;
 }
 
 const getInviterName = async (username) => {
