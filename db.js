3ea8366cfa98589a38644b2ef7291c6da138063e
Maksim K <woodoomax123@gmail.com>
2021-06-30 12:15:10 +0300

    git fixes

--- db.js
+++ db.js
@@ -2,15 +2,9 @@ const { Client } = require('pg');
 
 const client = new Client({
   user: 'apps',
-<<<<<<< HEAD
   host: 'apps-database.cluster-c0w0nxnsuhxv.eu-north-1.rds.amazonaws.com',
   database: 'apps',
   password: '4Y5FvarAv3hT69B',
-=======
-  host: 'localhost',
-  database: 'apps',
-  password: 'UsdXRHQFU2LPu2KXnNjz',
->>>>>>> 62e31abc236f76d63cbfe1fbe849ada25c9d7725
   port: 5432,
 });
 
--- server/controllers/connect/redirect/check-hash.js
+++ server/controllers/connect/redirect/check-hash.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const client = require('../../../../db');
 
 module.exports = async (req, res) => {
@@ -47,57 +46,3 @@ const deleteHash = (id) => {
 
   client.query(query);
 };
-=======
-const client = require("../../../../db");
-
-module.exports = async (req,res) => {
-    let {
-        hash
-    } = req.body;
-
-    const get_user_by_hash = `SELECT * FROM redirect WHERE hash = '${hash}'`;
-
-    const response = await client.query(get_user_by_hash);
-
-    if (response.rowCount === 0) {
-        return res.json({
-            status: 'error',
-            text: 'Not Found'
-        })
-    }
-
-    if (new Date().getTime() - new Date(response.rows[0].datetime).getTime() > 1000 * 15) {
-        deleteHash(response.rows[0].id);
-        res.json({
-            status: 'error',
-            text: 'Timeout'
-        })
-    }
-
-    const get_username = `SELECT username FROM quasar_telegrambot_users_new WHERE id = ${response.rows[0].user_id}`;
-
-    let username = await client.query(get_username);
-
-    if (username.rowCount === 0) {
-        deleteHash(response.rows[0].id);
-        return res.json({
-            status: 'error',
-            text: 'User ID not valid'
-        })
-    }
-
-    res.json({
-        status: 'ok',
-        username: username.rows[0].username
-    })
-
-    deleteHash(response.rows[0].id);
-    return;
-}
-
-const deleteHash = (id) => {
-    const query = `DELETE FROM redirect WHERE id = ${id}`;
-
-    client.query(query);
-} 
->>>>>>> 62e31abc236f76d63cbfe1fbe849ada25c9d7725
